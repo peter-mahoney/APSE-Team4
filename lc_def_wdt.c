0ac976b93fd39d089d8977c23b14dd80abf8f4cd
Joseph Jablonski <jjablon8@jh.edu>
2023-10-22 00:47:48 -0400

    lowering cap high temp

--- lc_def_wdt.c
+++ lc_def_wdt.c
@@ -363,7 +363,7 @@ LC_WDTEntry_t LC_DefaultWDT[LC_MAX_WATCHPOINTS] =
         .BitMask                    = LC_NO_BITMASK,
         .CustomFuncArgument         = 0,
         .ResultAgeWhenStale         = 0,
-        .ComparisonValue.Unsigned32 = 85,
+        .ComparisonValue.Unsigned32 = 80,
     },
 
     /* #19 CAP B High Charge */
@@ -375,7 +375,7 @@ LC_WDTEntry_t LC_DefaultWDT[LC_MAX_WATCHPOINTS] =
         .BitMask                    = LC_NO_BITMASK,
         .CustomFuncArgument         = 0,
         .ResultAgeWhenStale         = 0,
-        .ComparisonValue.Unsigned32 = 85,
+        .ComparisonValue.Unsigned32 = 80,
     },
 
     /* #20 CAP A Critical High Charge */
